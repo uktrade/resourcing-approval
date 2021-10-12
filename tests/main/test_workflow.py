02890b95e951907ef8b7eb21d5ee50be08bf2773
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-10-12 11:29:46 +0100

    CAP-16 Add job description step (#7)

--- tests/main/test_workflow.py
+++ tests/main/test_workflow.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import pytest
 from django.contrib.auth import get_user_model
 from django.test import Client
@@ -34,6 +36,13 @@ def approval(client):
     return create_approval(client)
 
 
+def continue_approval_workflow(client, approval, data):
+    return client.post(
+        reverse("flow-continue", args=[approval.flow.pk]),
+        {"uuid": approval.flow.current_task_record.uuid, **data},
+    )
+
+
 class TestContractorApprovalWorkflow:
     def test_create(self, db):
         approval_count = ContractorApproval.objects.count()
@@ -46,11 +55,34 @@ class TestContractorApprovalWorkflow:
     def test_query_ir35(self, client, approval):
         assert approval.is_ir35 is None
 
-        client.post(
-            reverse("flow-continue", args=[approval.flow.pk]),
-            {"uuid": approval.flow.current_task_record.uuid, "is_ir35": True},
-        )
+        continue_approval_workflow(client, approval, {"is_ir35": True})
 
         approval.refresh_from_db()
 
         assert approval.is_ir35 is True
+
+    def test_job_description(self, client, approval):
+        continue_approval = partial(continue_approval_workflow, client, approval)
+
+        continue_approval({"is_ir35": True})
+
+        assert approval.flow.current_task_record.task_name == "job_description"
+        assert approval.flow.current_task_record.step_id == "job_description"
+
+        role_purpose = "Develop new Django application."
+
+        continue_approval(
+            {
+                "title": "Senior Python Developer",
+                "role_purpose": role_purpose,
+                "key_accountabilities": "Write lots of great Python code.",
+                "line_management_responsibility": "Remove any blockers.",
+                "personal_attributes_and_skills": "Works well in a team.",
+                "essential_and_preferred_experience": "Written a lot of Python code.",
+            }
+        )
+
+        assert approval.job_description.role_purpose == role_purpose
+
+        approval.flow.refresh_from_db()
+        assert approval.flow.finished
