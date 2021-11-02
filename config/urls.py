8aeb879abdad84e9477c7bc7e45b5f3b21e1687b
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-11-02 12:56:12 +0000

    RAP-47 Remove admin login and set password pages

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,19 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path("", include("main.urls")),
     path("auth/", include("authbroker_client.urls")),
     path("dev-tools/", include("dev_tools.urls")),
+    # Remove admin login page.
+    path("admin/login/", RedirectView.as_view(url=settings.LOGIN_URL)),
+    # Remove admin set password page.
+    path(
+        "admin/user/user/<int:user_id>/password/",
+        RedirectView.as_view(url=reverse_lazy("index")),
+    ),
     path("admin/", admin.site.urls),
 ]
