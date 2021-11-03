4f793ffec52493e96df2326ef6cf97cfd165a305
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-11-03 13:56:41 +0000

    RAP-59 Add django-chunk-upload-handlers

--- config/settings/paas.py
+++ config/settings/paas.py
@@ -2,3 +2,14 @@
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# django-storages
+# https://django-storages.readthedocs.io/en/latest/
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# django-chunk-upload-handlers
+# https://github.com/uktrade/django-chunk-s3-av-upload-handlers
+FILE_UPLOAD_HANDLERS = (
+    # "django_chunk_upload_handlers.clam_av.ClamAVFileUploadHandler",
+    "django_chunk_upload_handlers.s3.S3FileUploadHandler",
+)  # Order is important
