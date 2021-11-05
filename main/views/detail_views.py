10928bb896d81cc50fbb41673cf6d407a692d279
Luisella Strona <luisella21@hotmail.com>
2021-11-05 12:20:37 +0000

    Added view details.

--- main/views/detail_views.py
+++ main/views/detail_views.py
@@ -1,5 +1,10 @@
 from main.views.supporting_forms import SupportingFormDetailView
-from main.models import JobDescription, InterimRequest, CestRationale
+from main.models import (
+    JobDescription,
+    InterimRequest,
+    CestRationale,
+    SdsStatusDetermination,
+)
 
 
 class JobDescriptionDetailView(SupportingFormDetailView):
@@ -18,4 +23,9 @@ class CestRationaleDetailView(SupportingFormDetailView):
     model = CestRationale
     permission_required = "main.view_cestrationale"
     title = "CEST Rationale"
+
+class SdsStatusDeterminationDetailView(SupportingFormDetailView):
+    model = SdsStatusDetermination
+    permission_required = "main.view_sdsstatusdetermination"
+    title = "SDS Status Determination"
 
