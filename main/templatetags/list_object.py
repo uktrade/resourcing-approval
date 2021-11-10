f483e4fb3fce831448241f9fd23e44e7ae0ca406
Luisella Strona <luisella21@hotmail.com>
2021-11-10 12:01:28 +0000

    Improved the template.

--- main/templatetags/list_object.py
+++ main/templatetags/list_object.py
@@ -3,10 +3,11 @@ from django import template
 
 register = template.Library()
 
+
 @register.inclusion_tag("main/partials/detail_list.html")
 def object_as_list(object, exclude_list):
-    data = []
+    data = {}
     for field in object._meta.fields:
         if field.name not in exclude_list:
-            data.append((field.verbose_name.capitalize(), getattr(object, field.name)))
+            data[field.verbose_name.capitalize()] = getattr(object, field.name)
     return {"display_list": data}
