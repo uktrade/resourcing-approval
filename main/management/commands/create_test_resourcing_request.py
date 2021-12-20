a95d8ae21dc214f7ce3485237197494e45c82d88
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-12-20 10:30:36 +0000

    RAP-162 Add optional portfolio field

--- main/management/commands/create_test_resourcing_request.py
+++ main/management/commands/create_test_resourcing_request.py
@@ -43,6 +43,7 @@ class Command(BaseCommand):
             type=ResourcingRequest.Type.NEW,
             job_title=job_title,
             project_name=project_name,
+            portfolio="Testing",
             profession_id=1,  # Development
             start_date=datetime.date.today(),
             end_date=datetime.date.today() + datetime.timedelta(days=30 * 6),
